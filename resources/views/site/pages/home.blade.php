1fb5ad1f2420e6e4d7736c06a11e674d97271a9e
amostafasyed <amostafasyed@gnail.com>
2020-12-09 02:48:05 +0600

    2nd commit--david

--- resources/views/site/pages/home.blade.php
+++ resources/views/site/pages/home.blade.php
@@ -10,6 +10,7 @@
         @else
             @include('site.home-partials.header')
         @endif
+
         @include('site.home-partials.nav-bar')
     </div>
     @include('site.home-partials.banner')
@@ -26,5 +27,38 @@
 
 
 @section('scripts')
+    <script>
+
+        function submit(id){
+            console.log(id);
+            var token = "{{ csrf_token() }}";
+            var qty=$('#qty').val();
+            $.ajax({
+                url: '/add-cart',
+                headers: {
+                    'X-CSRF-TOKEN': $('meta[name="_token"]').attr('content')
+                },
+                type: 'POST',
+                cache: false,
+                dataType: 'json',
+                data: {
+                    _token: token,
+                    id:id,
+                    qty:qty,
+                },
+                success: function (data)
+                {
+                    console.log(data);
+                    $("#counter").empty();
+                    $message=data;
+                    $("#counter").append($message);
+
+
+                }
+            })
+
+        }
+
+    </script>
 
 @endsection
